24513bb3ba94120e00ee14ce09409d1385405efd
Contato Fluenglish <contato@fluenglish.com.br>
2022-07-10 20:39:09 -0300

    add test

---
+++ tests/userService.test.ts
@@ -0,0 +1,26 @@
+// import { describe, beforeEach, test, expect, jest, it } from '@jest/globals'
+
+// import { createMock } from 'ts-auto-mock';
+
+// interface Interface {
+//     a: string;
+//     b: number;
+// }
+// test('teste', () => {
+//     let mock: Interface;
+//     mock = createMock<Interface>();
+
+
+
+
+// });
+
+
+// // test("Test singleton override - ok", () => {
+// //     let userRepository = createMock<IUserRepository>();
+
+// //     const userService = new UserService(userRepository);
+// //     expect(userService.save(new User("Thiago S Adriano"))).toEqual("Usuario: Thiago S Adriano")
+
+// // });
+
